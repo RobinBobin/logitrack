c2cb1766767500d28fae915d1a497258e02a186a
Ruben Shalimov <r_shalimov@inbox.ru>
2026-08-07 02:42:19 +0300

    feat(backend): set up a nodejs server to serve a hello world page

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello from monorepo Docker')
+import http from 'http'
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+  // Проверяем, что метод GET и путь /
+  if (req.method === 'GET' && req.url === '/') {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+    res.end('Hello from Logitrack with Docker!')
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Not Found')
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`Сервер успешно запущен на порту ${PORT}`)
+})
