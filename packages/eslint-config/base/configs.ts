8ee45ce65b9b064319f4b0031e562fb4c5ebf0db
Ruben Shalimov <r_shalimov@inbox.ru>
2026-08-08 14:02:32 +0300

    chore(eslint-config): add eslint-config-turbo and eslint-plugin-prettier

--- packages/eslint-config/base/configs.ts
+++ packages/eslint-config/base/configs.ts
@@ -1,12 +1,15 @@
 import eslintJs from '@eslint/js'
 import stylisticPlugin from '@stylistic/eslint-plugin'
 import { defineConfig } from 'eslint/config'
+import eslintConfigTurbo from 'eslint-config-turbo/flat'
 import { flatConfigs as importXFlatConfigs } from 'eslint-plugin-import-x'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 // @ts-expect-error Could not find a declaration file for module 'eslint-plugin-promise'.
 import eslintPluginPromise from 'eslint-plugin-promise'
 import { configs as typescriptEslintConfigs } from 'typescript-eslint'
 
 export const configs = defineConfig([
+  eslintConfigTurbo,
   eslintJs.configs.recommended,
   importXFlatConfigs.recommended,
   importXFlatConfigs.typescript,
@@ -15,5 +18,6 @@ export const configs = defineConfig([
   stylisticPlugin.configs['disable-legacy'],
   stylisticPlugin.configs.recommended,
   typescriptEslintConfigs.strictTypeChecked,
-  typescriptEslintConfigs.stylisticTypeChecked
+  typescriptEslintConfigs.stylisticTypeChecked,
+  eslintPluginPrettier
 ])
